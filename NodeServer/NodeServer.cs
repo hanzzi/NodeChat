e2afa422e32248348b0e0cb719ea04d499019ff3
Hans Hedegaard Byager <hans-byager@live.dk>
2018-10-06 17:00:58 +0200

    Add project files.

---
+++ NodeServer/NodeServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NodeUtil
+{
+    public class NodeServer
+    {
+        public IPAddress LocalIP;
+        public Dictionary<IPAddress, string> ClientPool;
+
+        public NodeServer()
+        {
+
+        }
+
+    }
+}
